7b83401d13f83ca722fdf28e4696dc029ddbceef
徐昊 <@DESKTOP-QH1I9H0>
2017-11-06 15:44:33 +0800

    改了下注释

--- src/segWinAPP/SegWin.java
+++ src/segWinAPP/SegWin.java
@@ -69,7 +69,7 @@ public class SegWin {
 			public void actionPerformed(ActionEvent arg0) {	//使用actionPerformed而非mouseclicked，因为后者只关注鼠标点击事件，甚至在按钮被禁用后仍会响应
 				/*
 				 * 打开文件按钮的操作响应事件
-				 * 将文件打开flag设置为true
+				 * 清屏
 				 * 打开一个文件选择窗口，选择一个txt文件，获得指向此文件的对象
 				 * 读取其中文本，将文本添加到textInputArea的text区域中
 				 */
@@ -84,11 +84,9 @@ public class SegWin {
 			public void actionPerformed(ActionEvent arg0) {
 				/*
 				 * 处理文本事件的操作响应事件
-				 * 清屏，置打开文件flag为false，清空两个textArea的text
 				 * 调用ICTCLAS分词接口进行分词和词性分析
-				 * 若打开文件flag为true，使用文件分词接口对文件对象进行分词
-				 * 否则使用字符串分词接口对textInputArea的text分词
-				 * 结果添加到textOutputArea的text
+				 * 对textInputArea的getText分词
+				 * 结果添加到textOutputArea的setText
 				 */
 				segProcess sp = new segProcess();
 				sp.inputSeg(textInputArea.getText());
